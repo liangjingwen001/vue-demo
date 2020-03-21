51587434582f72330f807deec3306928ff347b3e
liangjingwen <1591998442@qq.com>
2020-03-21 11:22:31 +0800

    自适应宽高

--- src/App.vue
+++ src/App.vue
@@ -39,6 +39,7 @@ export default {
 </script>
 
 <style>
+html,body{margin: 0; padding: 0}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/view/index.vue
+++ src/view/index.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="hello">
-        我是首页
+        <div class="title">我是首页</div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
 import { mapActions } from 'vuex'
 
-let ajax = axios.create();
 export default {
   name: 'HelloWorld',
   data () {
@@ -22,33 +20,33 @@ export default {
         this.$router.push({name: 'detail'})
     },
     init() {
-    // this.$ajax.addUser({
-    //   params: {
-    //     userName: '迪迦',
-    //     passWord: '光之子',
-    //     age: 999
-    //   },
-    //   success: (res) => {
-    //     console.log(res)
-    //   },
-    //   error: (err) => {
-    //     console.log(err)
-    //   }
-    // })
+      this.$ajax.addUser({
+        params: {
+          userName: '迪迦',
+          passWord: '光之子',
+          age: 999
+        },
+        success: (res) => {
+          console.log(res)
+        },
+        error: (err) => {
+          console.log(err)
+        }
+      })
 
-    this.$ajax.getUser({
-      params: {
-        us: '迪迦'
-      },
-      success: (res) => {
-        console.log(res)
-      },
-      error: (err) => {
-        console.log(err)
+      this.$ajax.getUser({
+        params: {
+          us: '迪迦'
+        },
+        success: (res) => {
+          console.log(res)
+        },
+        error: (err) => {
+          console.log(err)
+        }
+      })
       }
-    })
-    }
-  },
+    },
   created() {
     this.init();
     this.testAction()
@@ -58,5 +56,9 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
+$titleColor: blue;
+.hello{color: red;
+  .title{color: $titleColor; font-size: 2rem; width: 20rem; background-color: #eee; display: inline-block; height: 10rem}
+}
 </style>
